eb95ea02d461f7b71717b2c0cefd673fd3fb68cd
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2024-10-19 03:28:43 -0700

    route.ts

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -5,6 +5,9 @@ export async function GET() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuotes = quotes[randomIndex];
 
+    console.log("Random Index:", randomIndex); // Log the random index
+    console.log("Selected Quote:", randomQuotes); // Log the selected quote
+
     const response = NextResponse.json({ randomQuotes });
     response.headers.append('Cache-Control', 'no-store'); // Prevent caching
     return response;
